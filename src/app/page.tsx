8aa709f1a29a70d0c9ef14121aa161fe769a7129
arthursvpb <arthurandroid15@gmail.com>
2023-10-30 19:11:30 -0300

    feat(home): add initial homepage with favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import Image from 'next/image';
+
 export default function Home() {
 	return (
-		<main className="flex justify-center min-h-screen mx-auto">
+		<main className="flex flex-col items-center min-h-screen mx-auto">
 			<h1 className="text-3xl font-bold my-10">Arthur Vasconcellos</h1>
+			<Image
+				src="/assets/profile.jpg"
+				alt="Arthur's Profile Picture"
+				className="rounded-full border-2 border-green-300"
+				width={125}
+				height={125}
+				priority
+			/>
 		</main>
 	);
 }
