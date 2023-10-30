6d953a4f6d7097c77338f56930114e5837cc922f
arthursvpb <arthurandroid15@gmail.com>
2023-10-30 18:58:58 -0300

    add base page layout

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+	return (
+		<main className="flex justify-center min-h-screen mx-auto">
+			<h1 className="text-3xl font-bold my-10">Arthur Vasconcellos</h1>
+		</main>
+	);
+}
