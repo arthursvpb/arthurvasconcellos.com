abd0ca213fcff77246911b29599cfad862cbcd4a
arthursvpb <arthurandroid15@gmail.com>
2023-10-31 18:38:52 -0300

    global changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,13 @@ import { Inter } from 'next/font/google';
 import { ReactNode } from 'react';
 
 import '../styles/globals.css';
-import '../styles/custom.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function RootLayout({ children }: { children: ReactNode }) {
-	return (
-		<html lang="en-us">
-			<body className={inter.className}>{children}</body>
-		</html>
-	);
-}
+const RootLayout = ({ children }: { children: ReactNode }) => (
+	<html lang="en-us">
+		<body className={inter.className}>{children}</body>
+	</html>
+);
+
+export default RootLayout;
